5d4347dfd3ddfe6ddb1965f260d73f5a93645e53
saurabh chauhan <sourabh1202@gmail.com>
2025-04-21 06:00:42 +0300

    added gateway

---
+++ gateway/src/main/java/com/ecomstore/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.ecomstore.gateway;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
